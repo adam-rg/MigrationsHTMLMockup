77b44ca3b8d0b79c078b82554cd2542e051a28ba
Beth Aitman <beth.aitman@red-gate.com>
2015-11-20 11:59:32 +0000

    Make commit grid appear

--- js/controllers.js
+++ js/controllers.js
@@ -10,7 +10,46 @@ searchApp.controller('searchCtrl', function ($scope, $http) {
   });
   
   $scope.menuList = ["File", "Edit", "View", "Tools", "Window", "Community", "Help"];
+  $scope.objectExplorerContents = ["Server", "Databases", "System Databases", "Adventureworks", "Development", "MigTeamDev", "MigTeamTest", "MigTeamStaging", "MigTeamProd"];
+  $scope.ssmsTabs = [{
+    "name": "SQLQuery1.sql",
+    "class": "disabled"
+  },{
+    "name": "SQLQuery2.sql",
+    "class": "disabled"
+  },{
+    "name": "SQL Source Control",
+    "class": "active"
+  }];
 
+  $scope.socTabs = [{
+    "name": "Commit changes",
+    "class": "active"
+  },{
+    "name": "Get latest",
+    "class": ""
+  },{
+    "name": "Migrations",
+    "class": ""
+  },{
+    "name": "Object locking",
+    "class": ""
+  },{
+    "name": "Setup",
+    "class": ""
+  }];
+
+  $scope.commitGridContents = [{
+    "changeType": "Drop",
+    "name": "Foo",
+    "objectType": "Table",
+    "owner": "dbo"
+  },{
+    "changeType": "Edit",
+    "name": "ProductDescription",
+    "objectType": "Table",
+    "owner": "Production"
+  }];
   
   $scope.selectedRow = {};
   $scope.setRowSelection = function(result) {
